d343fedf8567699648671a6c2db76bcc47e51341
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-06-05 18:07:37 +0300

    :zap:Update file

--- schema.sql
+++ schema.sql
@@ -4,7 +4,7 @@ CREATE DATABASE vet_clinic;
 USE vet_clinic; 
 
 CREATE TABLE animals (
-    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    animalid INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name varchar(100),
     date_of_birth DATE,
     escape_attempts INT,
