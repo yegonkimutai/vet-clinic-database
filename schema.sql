82aa21823a9f65938ccbce86a89b18694bbd95f2
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-06-07 09:03:14 +0300

    :sparkles:Modify the schema.sql file

--- schema.sql
+++ schema.sql
@@ -12,4 +12,30 @@ CREATE TABLE animals (
     weight_kg DECIMAL(5, 2)
 );
 
-ALTER TABLE animals ADD COLUMN species varchar(100)
+ALTER TABLE animals ADD COLUMN species varchar(100);
+
+-- multiple tables
+
+-- species
+CREATE TABLE species(
+	id INT GENERATED ALWAYS AS IDENTITY,
+	name varchar(200),
+	PRIMARY KEY(id)
+);
+
+-- owners
+CREATE TABLE owners(
+	id INT GENERATED ALWAYS AS IDENTITY,
+	full_name varchar(200),
+    age INT,
+	PRIMARY KEY(id)
+);
+
+-- modify animals table
+ALTER TABLE animals DROP column species;
+
+ALTER TABLE animals ADD species_id INT REFERENCES species(id);
+
+ALTER TABLE animals ADD owners_id INT REFERENCES owners(id);
+
+
