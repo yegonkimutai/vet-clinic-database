d61f38fd1a846cb26a2a69350e765dfb17554210
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-06-08 16:05:07 +0300

    :recycle:Made changes suggested

--- data.sql
+++ data.sql
@@ -1,7 +1,7 @@
 /* Populate database with sample data. */
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES ('Augmon', '2020-02-03', '0', true, 10.23);
+VALUES ('Agumon', '2020-02-03', '0', true, 10.23);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES ('Gabumon', '2018-11-15', '2', true, 8);
@@ -56,7 +56,7 @@ SELECT * FROM animals;
 
 -- modify owners_id
 BEGIN;
-UPDATE animals SET owners_id = 1 WHERE name IN('Augmon');
+UPDATE animals SET owners_id = 1 WHERE name IN('Agumon');
 UPDATE animals SET owners_id = 2 WHERE name IN('Gabumon', 'Pikachu');
 UPDATE animals SET owners_id = 3 WHERE name IN('Devimon', 'Plantmon');
 UPDATE animals SET owners_id = 4 WHERE name IN('Charmander', 'Squirtle','Blossom');
@@ -66,7 +66,7 @@ SELECT * FROM animals;
 
 -- insert into vets
 INSERT INTO vets(name, age, graduation_date)
-VALUES('William Tatcher', 45, '2000-03-23'),
+VALUES('William Tatcher', 45, '2000-04-23'),
 ('Maisy Smith', 26, '2019-01-17'),
 ('Stephanie Mendez', 64, '1981-05-04'),
 ('Jack Harkness', 38, '2008-06-08')
@@ -80,8 +80,8 @@ VALUES((SELECT id FROM species WHERE name = 'Pokemon'),(SELECT id FROM vets WHER
 
 -- insert into visits
 INSERT INTO visits(animal_id, vet_id, date)
-VALUES((SELECT animalid FROM animals WHERE name = 'Augmon'), (SELECT id FROM vets WHERE name = 'William Tatcher'), '2020-05-24'),
-((SELECT animalid FROM animals WHERE name = 'Augmon'), (SELECT id FROM vets WHERE name = 'Stephanie Mendez'), '2020-07-22'),
+VALUES((SELECT animalid FROM animals WHERE name = 'Agumon'), (SELECT id FROM vets WHERE name = 'William Tatcher'), '2020-05-24'),
+((SELECT animalid FROM animals WHERE name = 'Agumon'), (SELECT id FROM vets WHERE name = 'Stephanie Mendez'), '2020-07-22'),
 ((SELECT animalid FROM animals WHERE name = 'Gabumon'), (SELECT id FROM vets WHERE name = 'Jack Harkness'), '2021-02-02'),
 ((SELECT animalid FROM animals WHERE name = 'Pikachu'), (SELECT id FROM vets WHERE name = 'Maisy Smith'), '2020-01-05'),
 ((SELECT animalid FROM animals WHERE name = 'Pikachu'), (SELECT id FROM vets WHERE name = 'Maisy Smith'), '2020-03-08'),
